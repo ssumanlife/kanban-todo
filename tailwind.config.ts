4ffeebc212c801ba68e80da8e35a1cd1a77ee8b6
ssumanlife <ksm9805@naver.com>
2025-02-14 15:27:53 +0900

    setting: 프로젝트 세팅 (#1)

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      colors: {
+        black: '#0a0a0a',
+        white: '#ffffff',
+        red: '#FF4040',
+        gray: {
+          100: '#ececec',
+          200: '#a7a7a7',
+          300: '#797979',
+          400: '#4d4d4d',
+          500: '#363636',
+          600: '#1d1d1d',
+        },
+      },
+      fontFamily: {
+        font: ['var(--font-pretend)', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config
