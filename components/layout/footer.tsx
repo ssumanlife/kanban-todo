5b2e1174ccc7c63ef195e62b71c96e497e37a83f
ssumanlife <ksm9805@naver.com>
2025-02-17 17:34:31 +0900

    feat: footer 추가 (#12)

---
+++ components/layout/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="p-[20px] fixed bottom-0 w-full bg-black text-center text-sm">
+      &copy; 2025. Kim Su Min. All rights reserved.
+    </footer>
+  )
+}
+
+export default Footer
