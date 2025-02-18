78c4f15a4985e98b1bfe6bf8e951c1c488b9d6c7
ssumanlife <ksm9805@naver.com>
2025-02-18 18:22:43 +0900

    design: skeleton 반응형 추가 (#28)

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -8,13 +8,13 @@ const Skeleton = () => {
 
   return (
     <div
-      className={`${commonStyle.wrapper} w-full h-[304px] p-6 flex justify-between items-center gap-4`}
+      className={`${commonStyle.wrapper} w-full md:h-[304px] h-[552px] p-6 flex justify-between items-center gap-4`}
     >
       <div className={`${commonStyle.item} w-[32px] h-[32px]`}></div>
       <div className={`${commonStyle.wrapper} w-full h-full p-2 flex flex-col gap-3`}>
         <div className={`${commonStyle.item} w-[220px] h-[36px]`}></div>
         <div className={`${commonStyle.item} w-[160px] h-[32px]`}></div>
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid md:grid-cols-3 grid-cols-1  gap-4">
           {Array.from({ length: 3 }, (_, index) => (
             <div key={index} className={`${commonStyle.item} h-[116px]`}></div>
           ))}
